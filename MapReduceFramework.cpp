4716da1d0944d278cdd985378512901a84da08a6
michal.maayan <mail@gmail.com>
2018-05-30 18:48:51 +0300

    Create the struct

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -2,8 +2,14 @@
 // Created by michal.maayan on 5/30/18.
 //
 
-
+#include "Barrier.h"
 #include "MapReduceClient.h"
+
+struct ThreadContext{
+    int threadId;
+    Barrier * barrier;
+    // , *atomicIndex, inVector, outVector, intermidiateVectorVector, queue, semaphore
+}
 
 void emit2 (K2* key, V2* value, void* context){
 
@@ -16,5 +22,5 @@ void emit3 (K3* key, V3* value, void* context){
 void runMapReduceFramework(const MapReduceClient& client,
                            const InputVec& inputVec, OutputVec& outputVec,
                            int multiThreadLevel){
-    
+
 }
