8314b50498ed6dfa728ade9cd320b3481cf06e5c
michal.maayan <mail@gmail.com>
2018-05-31 10:38:41 +0300

    emit2 and logic

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -9,36 +9,41 @@
 #include <pthread.h>
 
 
-struct ThreadContext{
+typedef struct ThreadContext{
     int threadId;
     int MT;
     Barrier * barrier;
     std::atomic<int>* atomicIndex;
     const InputVec* inputVec;
     OutputVec* outputVec;
-    IntermediateVec* vecOfVec;
+    IntermediateVec **arrayOfInterVec;
     const MapReduceClient* client;
 
     // , inVector, outVector, intermidiateVectorVector, queue, semaphore
-};
+}ThreadContext;
 
-struct MapContext{
+typedef struct MapContext{
+    IntermediateVec *interVector;
     
-};
+}MapContext;
 
 void emit2 (K2* key, V2* value, void* context){
-
+    auto* tc = (MapContext*) context;
+    tc->interVector->emplace_back(key, value);
 }
 void emit3 (K3* key, V3* value, void* context){
 
 }
 
 void* threadLogic (void* context){
-    ThreadContext* tc = (ThreadContext*) context;
+    auto* tc = (ThreadContext*) context;
     int oldValue;
     while(++*(tc->atomicIndex) < tc->MT) {
         oldValue = *(tc->atomicIndex) - 1;
-        map();
+        auto k1 = tc->inputVec->at(oldValue).first;
+        auto v1 = tc->inputVec->at(oldValue).second;
+        MapContext mapContext = {(tc->arrayOfInterVec)[oldValue]};
+        tc->client->map(k1, v1, &mapContext);
 
     }
     return 0;
@@ -51,11 +56,13 @@ void runMapReduceFramework(const MapReduceClient& client,
     ThreadContext contexts[multiThreadLevel];
     Barrier barrier(multiThreadLevel);
     std::atomic<int> atomicIndex(0);
-    IntermediateVec intermediateVec;
+    IntermediateVec* arrayOfInterVec[multiThreadLevel];
+    for (int i = 0; i < multiThreadLevel; ++i) {
+        arrayOfInterVec[i] = new IntermediateVec;
+    }
     for (int i = 0; i < multiThreadLevel; ++i) {
         //todo add initial of vec
-        contexts[i] = {i, multiThreadLevel, &barrier, &atomicIndex, &inputVec, &outputVec, &intermediateVec, &client};
-
+        contexts[i] = {i, multiThreadLevel, &barrier, &atomicIndex, &inputVec, &outputVec, arrayOfInterVec, &client};
     }
 
 
